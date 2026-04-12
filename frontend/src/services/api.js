80402d13b6e64e9a6cf3b508bc4af92ca2ecedf7
Aryan6174 <aaryansharma6174@gmail.com>
2026-04-12 17:10:12 +0530

    Update API_URL to include '/api' suffix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = (process.env.REACT_APP_API_URL || 'http://localhost:5000') + '/api';
 
 const API = axios.create({
   baseURL: API_URL,
