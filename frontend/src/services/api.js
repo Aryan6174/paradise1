72822009a5ead1ca559bb0b676cdcba8da055c8c
Aryan6174 <aaryansharma6174@gmail.com>
2026-04-12 10:37:24 +0530

    Update axios baseURL to use environment variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: REACT_APP_API_URL || "http://localhost:5000",
   headers: {
     'Content-Type': 'application/json'
   },
